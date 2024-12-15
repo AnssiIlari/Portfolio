c9585055f4fd82fb31e4009b750edf23c2b24152
Anssi Laitinen <korbikoski@gmail.com>
2024-12-15 17:04:23 +0200

    Basic contact form via Formspree

--- DecemberApp/src/App.jsx
+++ DecemberApp/src/App.jsx
@@ -3,6 +3,7 @@ import About from './components/About'
 import Projects from './components/Projects'
 import Skills from './components/Skills'
 import WorkExperience from './components/WorkExperience'
+import Contact from './components/Contact'
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
       <Projects />
       <Skills />
       <WorkExperience />
+      <Contact />
     </>
   )
 }
