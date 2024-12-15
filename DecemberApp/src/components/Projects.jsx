f23fa962b1abfd3bf4be5d37bb4c7146df498b7b
Anssi Laitinen <korbikoski@gmail.com>
2024-12-15 16:38:12 +0200

    Added project

--- DecemberApp/src/components/Projects.jsx
+++ DecemberApp/src/components/Projects.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 
 function Projects() {
-  return (
-    <section id="projects">
-      <h2>Projects</h2>
-      <div className="project">
-        <h3>Karelia christmas challenge 2023</h3>
-        <p>The competition's challenge involved working with a scrambled image and a CSV file. The primary task was to rearrange the scrambled image correctly using the clues provided in the CSV file.</p>
-        <a href="https://github.com/AnssiIlari/Karelia-Coding-Competition-2023" target="_blank" rel="noopener noreferrer">View on GitHub</a>
-      </div>
-    </section>
-  );
+    return (
+        <section id="projects">
+            <h2>Projects</h2>
+            <div className="ChristmasChallenge">
+                <h3>Karelia christmas challenge 2023</h3>
+                <p>The competition's challenge involved working with a scrambled image and a CSV file. The primary task was to rearrange the scrambled image correctly using the clues provided in the CSV file.</p>
+                <a href="https://github.com/AnssiIlari/Karelia-Coding-Competition-2023" target="_blank" rel="noopener noreferrer">View on GitHub</a>
+            </div>
+            <div className="Portfolio">
+                <h3>Portfolio</h3>
+                <p>This page right here. Yes that's right!</p>
+                <a href="https://github.com/AnssiIlari" target="_blank" rel="noopener noreferrer">ToBeAddedLater</a>
+            </div>
+        </section>
+    );
 }
 
 export default Projects;
