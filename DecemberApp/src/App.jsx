9dbf435784ca5bf9a6bcd23f17bf6d96dba45b0d
Anssi Laitinen <korbikoski@gmail.com>
2024-12-15 16:44:48 +0200

    Work experience started

--- DecemberApp/src/App.jsx
+++ DecemberApp/src/App.jsx
@@ -2,15 +2,18 @@ import './App.css'
 import About from './components/About'
 import Projects from './components/Projects'
 import Skills from './components/Skills'
+import WorkExperience from './components/WorkExperience'
 
 function App() {
 
   return (
     <>
-      <h1>Anssi Laitinen</h1>
+      <h1>Welcome!
+      </h1>
       <About />
       <Projects />
       <Skills />
+      <WorkExperience />
     </>
   )
 }
---
+++ DecemberApp/src/components/WorkExperience.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function WorkExperience() {
+  return (
+    <section id="work-experience">
+      <h2>Work Experience</h2>
+      <div className="job1">
+        <h3>Web Developer Trainee</h3>
+        <p>September 2024 - Present</p>
+        <p>Developed and maintained websites (PHP, CMS)</p>
+        <p>Designed, developed, and maintained mobile applications (React Native, JavaScript, TypeScript)</p>
+        <p>Managed server environments (Linux, Ubuntu, Nginx)</p>
+      </div>
+      <div className="job2">
+        <h3>Waste Truck Driver</h3>
+        <p>January 2013 - August 2024</p>
+        <p>Managed daily waste collection routes</p>
+      </div>
+    </section>
+  );
+}
+
+export default WorkExperience;
