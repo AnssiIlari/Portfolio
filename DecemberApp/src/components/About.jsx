093c8fd6d3dd86feb8e3ed55904f067c0bc586bd
Anssi Laitinen <korbikoski@gmail.com>
2024-12-20 19:54:54 +0200

    Modified About section

--- DecemberApp/src/components/About.jsx
+++ DecemberApp/src/components/About.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import { InView } from 'react-intersection-observer';
 import { motion } from 'framer-motion';
+import './About.css';
 
 function About() {
+
+  const aboutTextPart1 = `Welcome to my portfolio! I’m Anssi Laitinen, a soon-to-be graduate in Business Information Technology from Karelia University of Applied Sciences. Specializing in full-stack development, I have hands-on experience with languages and frameworks such as C#, Python, Java, JavaScript, TypeScript, React, and React Native. During my time as a Web Developer Trainee, I developed and maintained web and mobile applications and managed server environments.`;
+
+  const aboutTextPart2 = `I am passionate about continuous learning and thrive on solving real-world problems. Fluent in both Finnish and English. My goal is to build robust, scalable applications that make a meaningful impact and to grow continuously as a professional in the IT field.`;
+
+  const aboutTextPart3 = `I am currently seeking a full-time position as a Software Developer or similar role. If you have any questions or would like to discuss a potential project, feel free to contact me!`;
+
   return (
     <InView triggerOnce>
       {({ inView, ref }) => (
@@ -14,9 +22,9 @@ function About() {
               transition={{ duration: 2 }}
             >
               <h2>About Me</h2>
-              <p>
-                Welcome to my portfolio! I am a soon-to-graduate Bachelor of Business Information Technology.
-              </p>
+              <p className="about-text">{aboutTextPart1}</p>
+              <p className="about-text">{aboutTextPart2}</p>
+              <p className="about-text">{aboutTextPart3}</p>
             </motion.div>
           )}
         </section>
