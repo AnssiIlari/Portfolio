a25b729193795f85856e93d477308314fb57dd69
Anssi Laitinen <korbikoski@gmail.com>
2024-12-24 14:27:38 +0200

    Removed text

--- DecemberApp/src/components/About.jsx
+++ DecemberApp/src/components/About.jsx
@@ -7,8 +7,6 @@ function About() {
 
   const aboutTextPart1 = `Welcome to my portfolio! I’m Anssi Laitinen, a soon-to-be graduate in Business Information Technology from Karelia University of Applied Sciences. Specializing in full-stack development, I have hands-on experience with languages and frameworks such as C#, Python, Java, JavaScript, TypeScript, React, and React Native. During my time as a Web Developer Trainee, I developed and maintained web and mobile applications and managed server environments.`;
 
-  const aboutTextPart2 = `I am passionate about continuous learning and thrive on solving real-world problems. Fluent in both Finnish and English. My goal is to build robust, scalable applications that make a meaningful impact and to grow continuously as a professional in the IT field.`;
-
   const aboutTextPart3 = `I am currently seeking a full-time position as a Software Developer or similar role. If you have any questions or would like to discuss a potential project, feel free to contact me!`;
 
   return (
@@ -23,7 +21,6 @@ function About() {
             >
               <h2>About Me</h2>
               <p className="about-text">{aboutTextPart1}</p>
-              <p className="about-text">{aboutTextPart2}</p>
               <p className="about-text">{aboutTextPart3}</p>
             </motion.div>
           )}
