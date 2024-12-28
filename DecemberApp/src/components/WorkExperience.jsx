c19a5e24e30cc298c0bf8320dfcd0f37931c58dd
Anssi Laitinen <korbikoski@gmail.com>
2024-12-28 22:56:16 +0200

    Experience section styled

--- DecemberApp/src/components/WorkExperience.jsx
+++ DecemberApp/src/components/WorkExperience.jsx
@@ -2,23 +2,31 @@ import React from 'react';
 import { InView } from 'react-intersection-observer';
 import { motion } from 'framer-motion';
 
+import './WorkExperience.css';
+
 function WorkExperience() {
   const jobs = [
     {
       id: 1,
       title: 'Web Developer Trainee',
+      company: 'Molentum',
+      companyLink: 'https://molentum.fi/',
       duration: 'September 2024 - Present',
       responsibilities: [
         'Developed and maintained websites (PHP, CMS)',
         'Designed, developed, and maintained mobile applications (React Native, JavaScript, TypeScript)',
         'Managed server environments (Linux, Ubuntu, Nginx)',
       ],
+      backgroundImage: 'url(/molentum2.png)',
     },
     {
       id: 2,
       title: 'Waste Truck Driver',
+      company: 'Verdis',
+      companyLink: 'https://verdis.fi/',
       duration: 'January 2013 - August 2024',
       responsibilities: ['Managed daily waste collection routes'],
+      backgroundImage: 'url(/verdis.png)',
     },
     // Add more jobs as needed
   ];
@@ -26,28 +34,35 @@ function WorkExperience() {
   return (
     <section id="work-experience">
       <h2>Experience</h2>
-      {jobs.map((job, index) => (
-        <InView triggerOnce key={job.id}>
-          {({ inView, ref }) => (
-            <motion.div
-              ref={ref}
-              initial={{ opacity: 0, y: 50 }}
-              animate={inView ? { opacity: 1, y: 0 } : {}}
-              transition={{ duration: 2, delay: index * 0.2 }}
-              className={`job job${job.id}`}
-            >
-              <h3>{job.title}</h3>
-              <p>{job.duration}</p>
-              {job.responsibilities.map((responsibility, idx) => (
-                <p key={idx}>{responsibility}</p>
-              ))}
-            </motion.div>
-          )}
-        </InView>
-      ))}
+      <div className="experience-container">
+        {jobs.map((job, index) => (
+          <InView triggerOnce key={job.id}>
+            {({ inView, ref }) => (
+              <motion.div
+                ref={ref}
+                initial={{ opacity: 0, y: 50 }}
+                animate={inView ? { opacity: 1, y: 0 } : {}}
+                transition={{ duration: 0.5, delay: index * 0.2 }}
+                className="experience-card"
+                style={{ backgroundImage: job.backgroundImage }}
+              >
+                <div className="experience-content">
+                  <h3>{job.title}</h3>
+                  <span className="experience-duration">{job.duration}</span>
+                  {job.responsibilities.map((responsibility, idx) => (
+                    <p key={idx}>{responsibility}</p>
+                  ))}
+                </div>
+                <a href={job.companyLink} target="_blank" rel="noopener noreferrer" className="company-link">
+                  {job.company}
+                </a>
+              </motion.div>
+            )}
+          </InView>
+        ))}
+      </div>
     </section>
   );
 }
-
 
 export default WorkExperience;
